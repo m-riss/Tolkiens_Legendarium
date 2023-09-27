44102e154fe0e1dd14ac1e5871d0b016dab37f22
m-riss <95452643+m-riss@users.noreply.github.com>
2023-09-26 21:39:20 -0400

    Create strings.cpp

---
+++ strings.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+string J = "";
+string R = "";
+string Hyphen = "";
+string Left_Parent = "";
+string Right_Parent = "";
+string Forward_Slash = "";
+string Space = "";
+string Period = "";
+string Tolkien = "";
+string Colon = "";
+string by = "";
+string TOLKIEN = "";
+string Part = "";
+string Comma = "";
+string CONTENTS = "";
+string FOREWORD = "";
+string PROLOGUE = "";
+string weed = "";
+string Chapter = "";
+string APPENDICES = "";
+string QuestionMark = "";
+string F = "";
+string Strider = "";
+string INDEXES = "";
